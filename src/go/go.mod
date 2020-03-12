d861c9a0027888713f3a060b5bb0f779aa23d0cf
nvnamsss <nvnam.c@gmail.com>
2020-03-12 19:31:49 +0700

    try out with go

---
+++ src/go/go.mod
@@ -0,0 +1,10 @@
+module example.com/m/v2
+
+go 1.14
+
+require (
+	github.com/lukehoban/go-outline v0.0.0-20161011150102-e78556874252 // indirect
+	github.com/mdempsky/gocode v0.0.0-20191202075140-939b4a677f2f // indirect
+	github.com/uudashr/gopkgs/v2 v2.1.2 // indirect
+	golang.org/x/tools v0.0.0-20200312045724-11d5b4c81c7d // indirect
+)
---
+++ src/go/main.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+type Page struct {
+	pageNum int
+	title   string
+	data    []byte
+}
+
+func main() {
+	var pref *Page //like cpp, go separate reference and value by '*' symbol, it very similar to cpp by the way it initialize
+	pref = new(Page)
+	pref.pageNum = 2
+	pref.title = "hi mom reference"
+
+	var pvalue Page //create value for p, we do not need any constructor
+
+	pvalue.pageNum = 1
+	pvalue.title = "hi mom value"
+
+	var samevalue Page
+	samevalue = pvalue
+	samevalue.title = "hi mom samevalue"
+
+	var sameref *Page
+	sameref = pref
+	sameref.title = "hi mom sameref"
+
+	fmt.Println(sameref)
+	fmt.Println(samevalue)
+	fmt.Println(pref.title)
+	fmt.Println(pvalue.title)
+
+}
