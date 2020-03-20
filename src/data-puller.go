82dd3318298faffe229cbc5bd8e4277ba093ebe7
Nhựt <nhutcaucui@gmail.com>
2020-03-20 19:13:05 +0700

    connect database and simple query

--- src/data-puller.go
+++ src/data-puller.go
@@ -10,7 +10,7 @@ import (
 var Db *sql.DB
 
 func connect() {
-	db, err := sql.Open("mysql", "user:password@/dbname")
+	db, err := sql.Open("mysql", "ixPwflxQAF:3satzXbjYd@tcp(remotemysql.com)/ixPwflxQAF")
 	if err != nil {
 		fmt.Println("[Database]", "Error occurred", err)
 	}
@@ -19,5 +19,29 @@ func connect() {
 }
 
 func PrepareData() {
-	Db.Exec("Hi mom")
+	connect()
+
+	results, err := Db.Query("SELECT * FROM  nodes")
+
+	if err != nil {
+		panic(err.Error()) // proper error handling instead of panic in your app
+	}
+
+	for results.Next() {
+		var id int
+		var lat float32
+		var lon float32
+		var street string
+        err = results.Scan(&id,&lat,&lon,&street)
+        if err != nil {
+            panic(err.Error()) // proper error handling instead of panic in your app
+        }
+                // and then print out the tag's Name attribute
+		fmt.Println(id)
+		fmt.Println(lat)
+		fmt.Println(lon)
+		fmt.Println(street)
+	}
+	
+	fmt.Println(results)
 }
