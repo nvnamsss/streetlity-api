8b0fc9493f262167d77d67ecf461c68491d71e1b
nvnamsss <nvnam.c@gmail.com>
2020-03-28 12:39:46 +0700

    going to merge with master

--- src/router/router_fuel.go
+++ src/router/router_fuel.go
@@ -19,10 +19,13 @@ func getFuels(w http.ResponseWriter, req *http.Request) {
 		Message []string
 	}
 	result.Status = true
-	// var f model.Fuel
-	f := model.Db.Find(&model.Fuel{})
-	// model.Db.First(&f, 1)
-	fmt.Println(f)
+
+	var f []model.Fuel
+	model.Db.Find(&f)
+	result.Fuels = f
+
+	log.Println("[GetFuels]", f)
+
 	jsonData, jsonErr := json.Marshal(result)
 
 	if jsonErr != nil {
@@ -33,7 +36,46 @@ func getFuels(w http.ResponseWriter, req *http.Request) {
 }
 
 func getFuel(w http.ResponseWriter, req *http.Request) {
+	var result struct {
+		Status  bool
+		Fuels   []model.Fuel
+		Message []string
+	}
+	result.Status = true
+	result.Fuels = []model.Fuel{}
+	result.Message = []string{}
+	query := req.URL.Query()
 
+	var id int64
+	var idErr error
+	log.Println("[GetFuel]", query)
+	_, idReady := query["id"]
+	if !idReady {
+		result.Status = false
+		result.Message = append(result.Message, "Id is missing")
+	} else {
+		id, idErr = strconv.ParseInt(query["id"][0], 10, 64)
+		if idErr != nil {
+			result.Status = false
+			result.Message = append(result.Message, "Id is invalid")
+		}
+	}
+
+	if result.Status {
+		var f []model.Fuel
+		model.Db.Find(&f, id)
+		result.Fuels = f
+		log.Println("[GetFuel]", f)
+	} else {
+		log.Println("[GetFuel]", "Request failed")
+	}
+
+	jsonData, jsonErr := json.Marshal(result)
+
+	if jsonErr != nil {
+		log.Println(jsonErr)
+	}
+	w.Write(jsonData)
 }
 
 func update(w http.ResponseWriter, req *http.Request) {
@@ -87,7 +129,7 @@ func update(w http.ResponseWriter, req *http.Request) {
 func HandleFuel(router *mux.Router) {
 	log.Println("[Router]", "Handling fuel")
 	s := router.PathPrefix("/fuel").Subrouter()
-	s.HandleFunc("/getFuels", getFuels).
-		Methods("GET")
+	s.HandleFunc("/getFuels", getFuels).Methods("GET")
 	s.HandleFunc("/update", update).Methods("POST")
+	s.HandleFunc("/getFuel", getFuel).Methods("GET")
 }
