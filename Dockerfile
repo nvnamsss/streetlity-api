666a7f236b5db4417b76e802255702dfac1145e8
nvnamsss <nvnam.c@gmail.com>
2020-03-29 07:31:51 +0000

    modified Dockerfile and run script for fix with requirement

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM golang:latest
 
 ADD . /server
-WORKDIR /server
+WORKDIR /server/src
 RUN echo "Asia/Ho_Chi_Minh" > /etc/timezone
 
 CMD ["go", "run", "./main.go"]
--- run.sh
+++ run.sh
@@ -2,15 +2,15 @@
 name=$1
 
 echo "STOP RUNNING API CONTAINER"
-docker stop -t 30 {$name}_api_container 
-docker rm -f {$name}_api_container
+docker stop -t 30 ${name}_api_container 
+docker rm -f ${name}_api_container
 
 echo "DONE STOPPING"
 
-docker run --name {$name}_api_container \
-            --network common_net \
+docker run --name ${name}_api_container -d\
+            --network common-net \
             --restart always \
             -p 9000:9000 \
-            -d api_container:latest
+            api_containter
 
     
