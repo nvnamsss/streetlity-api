7ff07f4e0af073e71758c2dc5026ff0eddc8ccb9
nvnamsss <nvmam.c@gmail.com>
2020-05-12 13:31:37 +0700

    too tired

--- src/router/router_service_maintenance.go
+++ src/router/router_service_maintenance.go
@@ -266,7 +266,7 @@ func getMaintenance(w http.ResponseWriter, req *http.Request) {
 	pipe := pipeline.NewPipeline()
 	validateParams := pipeline.NewStage(func() (str struct{ Id int64 }, e error) {
 		ids, ok := query["id"]
-		if ok {
+		if !ok {
 			return str, errors.New("id param is missing")
 		}
 		var err error
@@ -285,7 +285,11 @@ func getMaintenance(w http.ResponseWriter, req *http.Request) {
 
 	if res.Status {
 		id := pipe.GetInt("Id")[0]
-		res.Maintenance, _ = model.MaintenanceById(id)
+		s, e := model.MaintenanceById(id)
+		res.Error(e)
+		if res.Status {
+			res.Maintenance = s
+		}
 	}
 
 	WriteJson(w, res)
@@ -374,6 +378,7 @@ func HandleMaintenance(router *mux.Router) {
 	s.HandleFunc("/all", getMaintenances).Methods("GET")
 	s.HandleFunc("/update", updateMaintenance).Methods("POST")
 	s.HandleFunc("/range", getMaintenanceInRange).Methods("GET")
+	s.HandleFunc("/id", getMaintenance).Methods("GET")
 	s.HandleFunc("/add", addMaintenance).Methods("POST")
 	s.HandleFunc("/order", orderMaintenance).Methods("POST")
 	s.HandleFunc("/accept", acceptOrderMaintenance).Methods("POST")
