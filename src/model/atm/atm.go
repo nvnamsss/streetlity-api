a25307163a5dde62d834ce3d48d6f77a78342f2b
nvnam <vinam@intra.co.kr>
2020-07-07 17:42:15 +0700

    replaced upvote by set confident

--- src/model/atm/atm.go
+++ src/model/atm/atm.go
@@ -195,8 +195,8 @@ func ImportByRawText(data string) (e error) {
 		}
 
 		s.Contributor = "Streetlity"
+		s.Confident = confident + 1
 		CreateService(s)
-		UpvoteServiceImmediately(s.Id)
 	}
 
 	return
